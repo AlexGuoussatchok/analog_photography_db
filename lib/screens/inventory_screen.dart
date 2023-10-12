8c5013dc39ab73aeb8cf7f21c1013d5f316ba17c
AlexGuoussatchok <115575196+AlexGuoussatchok@users.noreply.github.com>
2023-10-12 12:17:35 +0300

    + inventory tapBars modified

--- lib/screens/inventory_screen.dart
+++ lib/screens/inventory_screen.dart
@@ -8,47 +8,68 @@ class InventoryScreen extends StatefulWidget {
 }
 
 class _InventoryScreenState extends State<InventoryScreen>
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
+  late TabController _firstTabController;
   late TabController _secondTabController;
 
   @override
   void initState() {
     super.initState();
+    _firstTabController = TabController(length: 4, vsync: this);
     _secondTabController = TabController(length: 14, vsync: this);
   }
 
   @override
   void dispose() {
+    _firstTabController.dispose();
     _secondTabController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 4, // Control the first TabBar
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('My Inventory'),
-          bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(111.0),
-            child: Column(
-              children: [
-                const SizedBox(height: 10.0),
-                const Divider(height: 1.0, thickness: 1.0, color: Colors.white),
-                const TabBar(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Inventory'),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(120.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 10.0),
+              const Divider(height: 1.0, thickness: 3.0, color: Colors.white),
+              Container(
+                color: Colors.white,
+                child: TabBar(
+                  controller: _firstTabController,
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.grey,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  indicator: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(5),
+                  ),
                   tabs: [
-                    Tab(text: 'In Collection'),
+                    Tab(text: 'Collection'),
                     Tab(text: 'Wishlist'),
                     Tab(text: 'Sell-list'),
                     Tab(text: 'Borrowed'),
                   ],
                 ),
-                const SizedBox(height: 10.0),
-                const Divider(height: 1.0, thickness: 1.0, color: Colors.white),
-                TabBar(
+              ),
+              const SizedBox(height: 10.0),
+              const Divider(height: 1.0, thickness: 3.0, color: Colors.white),
+              Container(
+                color: Colors.white,
+                child: TabBar(
                   controller: _secondTabController,
                   isScrollable: true,
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.grey,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  indicator: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(5),
+                  ),
                   tabs: const [
                     Tab(text: 'Cameras'),
                     Tab(text: 'Lenses'),
@@ -66,15 +87,16 @@ class _InventoryScreenState extends State<InventoryScreen>
                     Tab(text: 'Photochemistry'),
                   ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-        body: TabBarView(
-          children: [
-            ...List.generate(4, (index) => const Center(child: Text('Content for selected tab.'))),
-          ],
-        ),
+      ),
+      body: TabBarView(
+        controller: _firstTabController,
+        children: [
+          ...List.generate(4, (index) => const Center(child: Text('Content for selected tab.'))),
+        ],
       ),
     );
   }
