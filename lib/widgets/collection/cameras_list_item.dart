48660fdba0c8fb0abaa3de95b976e04e8bd8cd32
AlexGuoussatchok <115575196+AlexGuoussatchok@users.noreply.github.com>
2023-10-14 15:44:25 +0300

    Little adjustments to camera details pop up

--- lib/widgets/collection/cameras_list_item.dart
+++ lib/widgets/collection/cameras_list_item.dart
@@ -10,36 +10,70 @@ class CameraListItem extends StatelessWidget {
   void _showCameraDetails(BuildContext context, InventoryCamera camera) {
     showDialog(
       context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text("${camera.brand} ${camera.model}"),
-          content: SingleChildScrollView(
-            child: ListBody(
+      builder: (context) => AlertDialog(
+        title: Text("${camera.brand} ${camera.model}"),
+        content: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.8, // 80% of screen width
+          height: MediaQuery.of(context).size.height * 0.5, // 50% of screen height
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('Serial Number: ${camera.serialNumber}'),
-                if (camera.purchaseDate != null) Text('Purchase Date: ${DateFormat('yyyy-MM-dd').format(camera.purchaseDate!)}'),
-                if (camera.pricePaid != null) Text('Price Paid: \$${camera.pricePaid}'),
-                Text('Condition: ${camera.condition}'),
-                if (camera.filmLoadDate != null) Text('Film Load Date: ${DateFormat('yyyy-MM-dd').format(camera.filmLoadDate!)}'),
-                if (camera.filmLoaded != null) Text('Film Loaded: ${camera.filmLoaded}'),
-                if (camera.averagePrice != null) Text('Average Price: \$${camera.averagePrice}'),
-                if (camera.comments != null && camera.comments!.isNotEmpty) Text('Comments: ${camera.comments}'),
-                // ... add other fields as needed
+                const SizedBox(height: 10),
+                if (camera.serialNumber != null)
+                  Text('Serial Number: ${camera.serialNumber}',
+                  style: const TextStyle(fontSize: 20)),
+
+                const SizedBox(height: 10),
+                if (camera.purchaseDate != null)
+                  Text('Purchase Date: ${DateFormat('yyyy-MM-dd').format(camera.purchaseDate!)}',
+                  style: const TextStyle(fontSize: 20)),
+
+                const SizedBox(height: 10),
+                if (camera.pricePaid != null)
+                  Text('Price Paid: ${camera.pricePaid}',
+                  style: const TextStyle(fontSize: 20)),
+
+                const SizedBox(height: 10),
+                if (camera.condition != null)
+                  Text('Condition: ${camera.condition}',
+                  style: const TextStyle(fontSize: 20)),
+
+                const SizedBox(height: 10),
+                if (camera.filmLoadDate != null)
+                  Text('Film Load Date: ${DateFormat('yyyy-MM-dd').format(camera.filmLoadDate!)}',
+                  style: const TextStyle(fontSize: 20)),
+
+                const SizedBox(height: 10),
+                if (camera.filmLoaded != null)
+                  Text('Film Loaded: ${camera.filmLoaded}',
+                  style: const TextStyle(fontSize: 20)),
+
+                const SizedBox(height: 10),
+                if (camera.averagePrice != null)
+                  Text('Average Price: \$${camera.averagePrice}',
+                  style: const TextStyle(fontSize: 20)),
+
+                const SizedBox(height: 10),
+                if (camera.comments != null)
+                  Text('Comments: ${camera.comments}',
+                  style: const TextStyle(fontSize: 20)),
               ],
             ),
           ),
-          actions: [
-            TextButton(
-              child: const Text('Close'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
+        ),
+        actions: [
+          TextButton(
+            child: const Text("Close"),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          )
+        ],
+      ),
     );
   }
+
 
 
   @override
